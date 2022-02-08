638bdea2dabb0429327eb6d882f85640a8cb99c0
Ishanamgai <ishanamgai1122@gmail.com>
2022-02-08 08:42:30 +0800

    no message

--- components/earnCard.js
+++ components/earnCard.js
@@ -5,10 +5,10 @@ export default function EarnCard() {
         <h1 className="text-5xl font-bold text-center mt-8">
           <span className="font-['NexaTextBlack']">0.25%</span>
         </h1>
-        <h1 className="font-['NexaTextBold'] text-center">Per Trade</h1>
+        {/* <h1 className="font-['NexaTextBold'] text-center">Per Trade</h1>
         <button className="bg-black text-white rounded-md font-bold text-sm mt-8 mb-4 py-2 px-5">
           Add Liquidity
-        </button>
+        </button> */}
       </div>
     );
   }
